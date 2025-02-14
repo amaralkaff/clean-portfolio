2c7b087cf2f206d7b07d643651c9877c4c20ef63
amaralkaff <tpamangly@gmail.com>
2025-02-14 23:56:53 +0800

    feat: add text discord

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/.well-known/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig 
